47c2f6502b42442d0136cf82907b3d20b410a7aa
mtf <m1501440483@email.swu.edu.cn>
2024-08-07 16:21:23 +0800

    generate exp81 raw configs, total:6

--- seg/configs/experiments/local-exp81/gta2cs_dacs_a999_fdthings_rcs001_cpl_m32-07-spta_dlv2red_r101v1c_poly10warm_s0.py
+++ seg/configs/experiments/local-exp81/gta2cs_dacs_a999_fdthings_rcs001_cpl_m32-07-spta_dlv2red_r101v1c_poly10warm_s0.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../../_base_/default_runtime.py',
-    '../../_base_/models/daformer_sepaspp_mitb5.py',
+    '../../_base_/models/deeplabv2red_r50-d8.py',
     '../../_base_/datasets/uda_gta_to_cityscapes_512x512.py',
     '../../_base_/uda/dacs_a999_fdthings.py',
     '../../_base_/schedules/adamw.py',
@@ -10,6 +10,8 @@ gpu_model = 'NVIDIAGeForceRTX4060Ti'
 n_gpus = 1
 seed = 0
 model = dict(
+    pretrained='open-mmlab://resnet101_v1c',
+    backbone=dict(depth=101),
     decode_head=dict())
 data = dict(
     samples_per_gpu=2,
@@ -28,20 +30,15 @@ uda = dict(
         type='block', mask_ratio=0.7, mask_block_size=32, _delete_=True))
 optimizer_config = None
 optimizer = dict(
-    lr=6e-05,
-    paramwise_cfg=dict(
-        custom_keys=dict(
-            head=dict(lr_mult=10.0),
-            pos_block=dict(decay_mult=0.0),
-            norm=dict(decay_mult=0.0))))
+    lr=6e-05, paramwise_cfg=dict(custom_keys=dict(head=dict(lr_mult=10.0))))
 runner = dict(type='IterBasedRunner', max_iters=40000)
-checkpoint_config = dict(by_epoch=False, interval=20000, max_keep_ckpts=1)
+checkpoint_config = dict(by_epoch=False, interval=40000, max_keep_ckpts=1)
 evaluation = dict(interval=4000, metric='mIoU')
-name = 'gta2cs_dacs_a999_fdthings_rcs001_cpl_m32-07-spta_daformer_sepaspp_mitb5_poly10warm_s0'
+name = 'gta2cs_dacs_a999_fdthings_rcs001_cpl_m32-07-spta_dlv2red_r101v1c_poly10warm_s0'
 exp = 81
 name_dataset = 'gta2cityscapes'
-name_architecture = 'daformer_sepaspp_mitb5'
-name_encoder = 'mitb5'
-name_decoder = 'daformer_sepaspp'
+name_architecture = 'dlv2red_r101v1c'
+name_encoder = 'r101v1c'
+name_decoder = 'dlv2red'
 name_uda = 'dacs_a999_fdthings_rcs0.01_cpl_m32-0.7-spta'
 name_opt = 'adamw_6e-05_pmTrue_poly10warm_1x2_40k'
